7d3487504fc6bcec6a204dffe3744900212b151e
Ilyes Safi <ilyes.safi@tek-up.tn>
2020-06-21 13:42:12 +0100

    first commit

---
+++ Algorithm_implementation.py
@@ -0,0 +1,51 @@
+""" Partie1 :Développement de l’algorithme de Validity Index"""
+import numpy as np
+from sklearn.cluster import KMeans
+
+
+#a Between Scatter matrix
+def BSX(i,X,Cluster_centers):
+    mean_1 = np.mean(X[0, :])
+    mean_2 = np.mean(X[1, :])
+    mean_X= np.array([[mean_1],[mean_2]])
+    bsx=np.zeros((2,2))
+    n=X.shape[0]/i
+    for j in range(i):
+        bsx += n*(Cluster_centers[j].reshape(2,1)-mean_X).dot((Cluster_centers[j].reshape(2,1)-mean_X).T)
+    return bsx
+
+#b Within Scatter matrix
+
+def WSX(i,X,Cluster_centers,pred_X):
+    Cluster_center = Cluster_centers[i]
+    wsx=np.zeros((2,2))
+    c=0
+    for j in X:
+        if pred_X[c] == i:
+            wsx+= (j.reshape(2,1)-Cluster_center).dot((j.reshape(2,1)-Cluster_center).T)
+        c+=1
+    return wsx
+
+
+
+
+#c Algorithme de Validity Index
+def Validity_Index(X):
+    vsc=[]
+    for i in range(2, 11):
+        kmeans = KMeans(n_clusters=i)
+        kmeans.fit(X)
+        Cluster_centers = kmeans.cluster_centers_
+        pred_X = kmeans.fit_predict(X)
+        bsx = BSX(i, X,Cluster_centers)
+        Sep_i = np.trace(bsx)
+        Comp_i = 0
+        for j in range(i):
+            wsx = WSX(j, X, Cluster_centers,pred_X)
+            trc = np.trace(wsx)
+            Comp_i += trc
+        Vsc = Sep_i / Comp_i
+        if i ==3:
+            print(Sep_i,Comp_i)
+        vsc.append(Vsc)
+    return vsc
---
+++ Simple_Application.py
@@ -0,0 +1,28 @@
+from Partie_1 import *
+from sklearn.cluster import KMeans
+
+""" Partie2 :Creatiion de base de données Artificielle"""
+
+import matplotlib.pyplot as plt
+from sklearn.datasets.samples_generator import make_blobs
+#2
+X,y=make_blobs(n_samples=600,n_features=2,centers=3)
+#3
+plt.scatter(X[:,0],X[:,1])
+plt.show()
+
+#5
+VX=Validity_Index(X)
+c_opt=max(VX)
+
+#6
+kmeans = KMeans(n_clusters=VX.index(c_opt)+2)
+kmeans.fit(X)
+
+#7
+
+
+
+#8
+plt.scatter(X[:,0], X[:,1], c=kmeans.labels_, cmap='rainbow')
+plt.scatter(kmeans.cluster_centers_[:,0] ,kmeans.cluster_centers_[:,1], color='black')
